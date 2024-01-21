5c078927ba4d3b7276154037fee83bf139779083
Dan Mills <evilhamsterman@gmail.com>
2024-01-20 19:09:30 -0800

    Mr Bear with sleep

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
 """Play music on Mr Bear"""
-import random
-import time
-from pathlib import Path
 
-import board
+from supervisor import reload
 
-from objects import LED, AudioOut, Button, SDCard
+from mr_bear import App, log
+
+app = App()
 
 if __name__ == "__main__":
-    sdcard = SDCard()
-    audio = AudioOut()
-    left_button = Button(board.TX)
-    right_button = Button(board.RX)
-    led = LED()
-    left_dir = Path("left")
-    right_dir = Path("right")
-
-    # Stop annoying regular blink LED
-    import supervisor
-
-    supervisor.runtime.rgb_status_brightness = 0
-
-    print("Loaded and ready")
-    led.blink(3)
-
-    while True:
-        if left_button.pressed:
-            file = random.choice(sdcard.ls_files(left_dir, ".mp3"))
-            audio.play(file, led)
-            time.sleep(0.1)
-        if right_button.pressed:
-            file = random.choice(sdcard.ls_files(right_dir, ".mp3"))
-            audio.play(file, led)
-            time.sleep(0.1)
+    log.info("Starting Mr Bear")
+    try:
+        app.run()
+    except Exception as e:
+        log.error(e)
+        app.led.blink(5)
+        reload()
--- mr_bear.py
+++ mr_bear.py
@@ -1,22 +1,27 @@
-import re
+import random
 import time
 from pathlib import Path
 
-import audiobusio
-import audiomp3
+import adafruit_logging as logging
+import alarm
 import board
 import digitalio
-import pwmio
 import sdcardio
-import storage
+import supervisor
+from audiobusio import I2SOut
+from audiomp3 import MP3Decoder
 from microcontroller import Pin
+from pwmio import PWMOut
+from storage import VfsFat, mount
+
+log = logging.getLogger("mr_bear")
 
 
 class LED:
     """LED object"""
 
     def __init__(self, pin: Pin = board.SDA) -> None:
-        self.led = pwmio.PWMOut(pin, frequency=1000, duty_cycle=0)
+        self.led = PWMOut(pin, frequency=1000, duty_cycle=0)
 
     def _convert_brightness(self, value: int) -> int:
         """Converts values between 0 and 255 to values between 0 and 65535"""
@@ -76,13 +81,9 @@ class SDCard:
 
     def __init__(self, mount_point: str | Path = Path("/sd")) -> None:
         self.mount_point = mount_point
-        print("Creating card")
         card = sdcardio.SDCard(board.SPI(), board.A0)
-        print("Loading Filesystem")
-        vfs = storage.VfsFat(card)
-        print("Mouning SD Card")
-        storage.mount(vfs, str(self.mount_point))
-        print("SD Card Mounted")
+        vfs = VfsFat(card)
+        mount(vfs, str(self.mount_point))
 
     def ls(self, folder: str | Path = None) -> list[Path]:
         """Returns a list of items in a folder"""
@@ -102,27 +103,39 @@ class SDCard:
             return [f for f in self.ls(folder) if f.is_file() and f.suffix == ext]
 
 
-class AudioOut:
+class Audio:
     """Audio Player"""
 
     def __init__(self) -> None:
-        self.audio = audiobusio.I2SOut(board.A3, board.A2, board.A1)
+        self.audio = I2SOut(board.A3, board.A2, board.A1)
+        self._decoder = None
 
-    def play(self, file: str | Path, led: LED | None = None):
-        """Play a WAV file"""
-        with open(str(file), "rb") as f:
-            wave = audiomp3.MP3Decoder(f)
-            self.audio.play(wave)
-            print(f"Playing {file}")
-            if led:
-                led.off()
-            while self.audio.playing:
-                if led:
-                    level = wave.rms_level * 3
-                    led.brightness = level
-                time.sleep(0.1)
-            led.off()
-            print("finished")
+    def play(self, file: str | Path):
+        """Play an MP# file"""
+        # Because everything else is unaware of Path objects
+        file = str(file)
+        if self._decoder is None:
+            self._decoder = MP3Decoder(file)
+        else:
+            self._decoder.open(file)
+        log.info(f"Playing {file}")
+        self.audio.play(self._decoder)
+
+    def stop(self):
+        """Stop playing"""
+        log.info("Stopping")
+        self.audio.stop()
+
+    @property
+    def playing(self) -> bool:
+        """Returns the current state of the audio"""
+        return self.audio.playing
+
+    def pulse_led(self, led: LED):
+        """Pulse the LED to the music"""
+        level = self._decoder.rms_level * 2
+        log.debug(f"RMS Level: {level}")
+        led.brightness = level
 
 
 class Button:
@@ -145,3 +158,49 @@ class Button:
 def normalize_scale(value: int, in_min: int, in_max: int, out_min: int, out_max: int):
     """Normalize a value from one scale to another"""
     return int((value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+
+class App:
+    """Main application class"""
+
+    def __init__(self):
+        self.sdcard = SDCard()
+        self.audio = Audio()
+        self.led = LED()
+
+        if supervisor.runtime.serial_connected:
+            log.setLevel(logging.INFO)
+            log.addHandler(logging.StreamHandler())
+        else:
+            log.setLevel(logging.ERROR)
+            log.addHandler(logging.FileHandler("/sd/mr_bear.log"))
+
+        # Stop annoying regular blink LED
+        supervisor.runtime.rgb_status_brightness = 0
+
+        log.info("Loaded and ready")
+
+    def run(self):
+        """Run the main loop"""
+        left_button = Button(board.RX)
+        right_button = Button(board.TX)
+        if alarm.wake_alarm:
+            file = random.choice(self.sdcard.ls_files("", ".mp3"))
+            self.audio.play(file)
+
+        while self.audio.playing:
+            log.info("Entering loop")
+            if left_button.pressed or right_button.pressed:
+                self.audio.stop()
+            if self.audio.playing:
+                self.audio.pulse_led(self.led)
+            time.sleep(0.1)
+        else:
+            log.info("Audio done")
+            self.led.off()
+            left_button.button.deinit()
+            right_button.button.deinit()
+            left_alarm = alarm.pin.PinAlarm(pin=board.RX, value=False, pull=True)
+            right_alarm = alarm.pin.PinAlarm(pin=board.TX, value=False, pull=True)
+            log.info("Entering deep sleep")
+            alarm.exit_and_deep_sleep_until_alarms(left_alarm, right_alarm)
